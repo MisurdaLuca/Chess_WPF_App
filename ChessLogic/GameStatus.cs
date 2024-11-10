a7ed625a78128ccf8714799519ef0b445b05b15b
Nyúltimátum <misurda.luca@gmail.com>
2024-11-10 11:46:06 +0100

    feat(ChessLogic): add GameStatus.cs

---
+++ ChessLogic/GameStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class GameStatus
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+        public GameStatus(Player player,Board board)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
