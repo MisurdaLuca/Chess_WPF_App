b924babe0b9aed5b7db490db983f4d0d98780467
Nyúltimátum <misurda.luca@gmail.com>
2024-11-10 15:58:49 +0100

    feat(ChessLogic\Pieces): add  MovePositions, GetMoves in King.cs

--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -11,6 +11,17 @@ namespace ChessLogic
         public override PieceType Type => PieceType.King;
 
         public override Player Color { get; }
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast,
+            Direction.NorthEast,
+        };
 
         public King(Player color)
         {
@@ -21,6 +32,30 @@ namespace ChessLogic
             King copy = new King(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach(Direction dir in dirs)
+            {
+                Position to = from + dir;
+                if(!Board.IsInside(to))
+                {
+                    continue;
+                }
+                if (board.IsEmpty(to) || board[to].Color!=Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach (Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
         }
     }
 }
