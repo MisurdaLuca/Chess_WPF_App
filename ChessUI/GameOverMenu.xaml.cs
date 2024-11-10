1531cc2234182a6707e10822ccc780284b660a00
Nyúltimátum <misurda.luca@gmail.com>
2024-11-10 19:38:43 +0100

    style(ChessUI): display of end-of-game subtitles for various reasons in GameOverMenu.xaml.cs

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,55 @@ namespace ChessUI
     /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+        public event Action<Option> OptionSelected;
+        public GameOverMenu(GameStatus gameStatus)
         {
             InitializeComponent();
+            Result result=gameStatus.Result;
+            WinnerText.Text=GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameStatus.CurrentPlayer);
         }
 
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "WHITE WINS!",
+                Player.Black => "BLACK WINS!",
+                _ => "IT'S A DRAW"
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "WHITE",
+                Player.Black => "BLACK",
+                _ => ""
+            };
+        }
+
+        private static string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Stalemate => $"STALEMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.Checkmate => $"CHECKMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.FiftyMoveRule => "FIFYT-MOVE RULE",
+                EndReason.InsufficentMaterial => "INSUFFICIENT MATERIAL",
+                EndReason.ThreefoldRepetition => "THREEFOLD REPETITION",
+                _ => ""
+            };
+        }
         private void Restart_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Restart);
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Exit);
         }
     }
 }
