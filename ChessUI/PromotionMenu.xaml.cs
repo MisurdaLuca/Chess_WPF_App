40d38c5bb2780d8569f029b9b55be563ea35df86
Nyúltimátum <misurda.luca@gmail.com>
2024-11-11 22:36:27 +0100

    feat(ChessUI): add events to PromotionMenu.xaml.cs

--- ChessUI/PromotionMenu.xaml.cs
+++ ChessUI/PromotionMenu.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using ChessLogic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessUI
 {
@@ -20,9 +9,34 @@ namespace ChessUI
     /// </summary>
     public partial class PromotionMenu : UserControl
     {
-        public PromotionMenu()
+        public event Action<PieceType> PieceSelected;
+        public PromotionMenu(Player player)
         {
             InitializeComponent();
+            QueenImg.Source=Images.GetImage(player, PieceType.Queen);
+            BishopImg.Source = Images.GetImage(player, PieceType.Bishop);
+            RookImg.Source = Images.GetImage(player, PieceType.Rook);
+            KnightImg.Source = Images.GetImage(player, PieceType.Knight);
+        }
+
+        private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Queen);
+        }
+
+        private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Bishop);
+        }
+
+        private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Knight);
+        }
+
+        private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Rook);
         }
     }
 }
